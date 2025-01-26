11860e2b0105f042e4c3381277d7081e9a7583d0
bigosofts <abdullah.limonbau@gmail.com>
2025-01-26 20:46:52 +0600

    Typescript Foundation

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-//npm i -g typescript
-//tsc index.ts
-//attach index.js to html in Script Tag before body
-
-console.log("Foundation of Typescript Lecture 1");
+//variable in Typescript
+//datatype is infered during initialization in typescript
+//only take letter, digit, underscrore and dollar sign for variable naming
+//first character not be a digit
+//variable names are case sensitive
+//typescript resereved name can not be used
+var num_one_1$ = 100;
+num_one_1$ = "hello23"; //error
+console.log(num_one_1$);
+var str = "Hello World!";
+console.log(str);
--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log("Foundation of Typescript 1");
+//variable in Typescript
+
+//datatype is infered during initialization in typescript
+
+//only take letter, digit, underscrore and dollar sign for variable naming
+
+//first character not be a digit
+
+//variable names are case sensitive
+
+//typescript resereved name can not be used
+
+let num_one_1$ = 100;
+num_one_1$ = "hello23"; //error
+console.log(num_one_1$);
+
+const str = "Hello World!";
+console.log(str);
