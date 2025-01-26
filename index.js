090f7ff0d3029eb18b5c0a61da294e241ca22eb2
bigosofts <abdullah.limonbau@gmail.com>
2025-01-26 23:28:50 +0600

    Typescript Foundation

--- index.js
+++ index.js
@@ -1,41 +1,6 @@
-//Typescript Enums Type
-//one way to make user role
-//------------------------------------
-// const ADMIN = 0;
-// const READ_ONLY = 1;
-// const WRITE_ONLY = 2;
-// const READ_WRITE = 3;
-// const user = {
-//   name: "John",
-//   age: 30,
-//   gender: "male",
-//   role: ADMIN,
-// };
-//-----------------------------------------
-//using Enum
-//----------------------------------------
-var Roles;
-(function (Roles) {
-    Roles[Roles["ADMIN"] = 0] = "ADMIN";
-    Roles[Roles["READ_ONLY"] = 1] = "READ_ONLY";
-    Roles[Roles["WRITE_ONLY"] = 2] = "WRITE_ONLY";
-    Roles[Roles["READ_WRITE"] = 3] = "READ_WRITE";
-})(Roles || (Roles = {})); // default index 0,1,2,3
-var Roles2;
-(function (Roles2) {
-    Roles2[Roles2["ADMIN"] = 100] = "ADMIN";
-    Roles2[Roles2["READ_ONLY"] = 200] = "READ_ONLY";
-    Roles2[Roles2["WRITE_ONLY"] = 300] = "WRITE_ONLY";
-    Roles2[Roles2["READ_WRITE"] = 400] = "READ_WRITE";
-})(Roles2 || (Roles2 = {})); //our custom index
-console.log(Roles);
-var user = {
-    name: "John",
-    age: 30,
-    gender: "male",
-    role: Roles2.ADMIN,
-};
-if (user.role === Roles2.ADMIN) {
-    console.log("This user is admin");
-}
-//we can access enum bidirectionally like Roles2.ADMIN or Roles2[100]
+//Typescript Any Type
+var dynamicData;
+dynamicData = {}; //can assign any type
+var arr;
+arr = [1, true]; //assign any element in array
+var test; // automatic any
--- index.ts
+++ index.ts
@@ -1,45 +1,11 @@
-//Typescript Enums Type
+//Typescript Any Type
 
-//one way to make user role
-//------------------------------------
-// const ADMIN = 0;
-// const READ_ONLY = 1;
-// const WRITE_ONLY = 2;
-// const READ_WRITE = 3;
+let dynamicData: any;
 
-// const user = {
-//   name: "John",
-//   age: 30,
-//   gender: "male",
-//   role: ADMIN,
-// };
-//-----------------------------------------
+dynamicData = {}; //can assign any type
 
-//using Enum
-//----------------------------------------
-enum Roles {
-  ADMIN,
-  READ_ONLY,
-  WRITE_ONLY,
-  READ_WRITE,
-} // default index 0,1,2,3
+let arr: any[];
 
-enum Roles2 {
-  ADMIN = 100,
-  READ_ONLY = 200,
-  WRITE_ONLY = 300,
-  READ_WRITE = 400,
-} //our custom index
+arr = [1, true]; //assign any element in array
 
-console.log(Roles)
-const user = {
-  name: "John",
-  age: 30,
-  gender: "male",
-  role: Roles2.ADMIN,
-};
-if (user.role === Roles2.ADMIN) {
-  console.log("This user is admin");
-}
-
-//we can access enum bidirectionally like Roles2.ADMIN or Roles2[100]
+let test; // automatic any
