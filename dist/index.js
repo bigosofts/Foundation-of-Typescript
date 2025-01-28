d66042105487a700059f085b6ca9d2810fe19de8
bigosofts <abdullah.limonbau@gmail.com>
2025-01-28 22:15:06 +0600

    Class Access modifier 2

--- dist/index.js
+++ dist/index.js
@@ -1,47 +1,6 @@
 "use strict";
-//Typescript Interface
-// let user1: User;
-// user1 = {
-//   firstName: "John",
-//   lastName: "Smith",
-//   greetUser() {
-//     console.log("hello user");
-//   },
-//   getFullName() {
-//     return this.firstName + this.lastName;
-//   },
-// };
-//class Admin implements User, Roles {} // can pass multiple interface. Class should have all the method and proeprty of the interface. but have additional method or property as well ....
-class Admin {
-    constructor(firstName, lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = 40;
-    }
-    greetUser() {
-        console.log("hello admin: " + this.getFullName());
-    }
-    getFullName() {
-        return this.firstName + this.lastName;
-    }
-}
-class Member {
-    constructor(firstName, lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = 20;
-    }
-    greetUser() {
-        console.log("hello member: " + this.getFullName());
-    }
-    getFullName() {
-        return this.firstName + this.lastName;
-    }
-}
-function displayGreetMessage(user) {
-    user.greetUser();
-}
-const admin = new Admin("Abdullah", "Limon");
-const member = new Member("Rabeya", "Akbor");
-displayGreetMessage(admin);
-displayGreetMessage(member);
+//Typescript Typecasting in tpescript
+let fName = document.getElementById("fName");
+// let fName = document.getElementById("fName")! as HTMLInputElement; //another way
+fName.value = "John";
+console.log(fName.value);
--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,8 @@
-//Typescript Interface
+//Typescript Typecasting in tpescript
 
-interface Person {
-  age: number;
-}
+let fName = <HTMLInputElement>document.getElementById("fName")!;
 
-interface User extends Person {
-  firstName: string;
-  lastName: string;
+// let fName = document.getElementById("fName")! as HTMLInputElement; //another way
 
-  greetUser(): void;
-  getFullName(): string;
-} //interface support interfce inheritance but type is not support that
-
-// let user1: User;
-
-// user1 = {
-//   firstName: "John",
-//   lastName: "Smith",
-//   greetUser() {
-//     console.log("hello user");
-//   },
-//   getFullName() {
-//     return this.firstName + this.lastName;
-//   },
-// };
-
-//class Admin implements User, Roles {} // can pass multiple interface. Class should have all the method and proeprty of the interface. but have additional method or property as well ....
-
-class Admin implements User {
-  age: number = 40;
-  constructor(public firstName: string, public lastName: string) {}
-  greetUser() {
-    console.log("hello admin: " + this.getFullName());
-  }
-  getFullName() {
-    return this.firstName + this.lastName;
-  }
-}
-
-class Member implements User {
-  age: number = 20;
-  constructor(public firstName: string, public lastName: string) {}
-  greetUser() {
-    console.log("hello member: " + this.getFullName());
-  }
-  getFullName() {
-    return this.firstName + this.lastName;
-  }
-}
-
-function displayGreetMessage(user: User) {
-  user.greetUser();
-}
-
-const admin = new Admin("Abdullah", "Limon");
-const member = new Member("Rabeya", "Akbor");
-
-displayGreetMessage(admin);
-displayGreetMessage(member);
+fName.value = "John";
+console.log(fName.value);
