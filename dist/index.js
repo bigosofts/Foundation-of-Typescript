2786efbf8b86d7d53113d11849bc7a07daea40bb
bigosofts <abdullah.limonbau@gmail.com>
2025-01-28 21:58:39 +0600

    Class Access modifier 2

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,47 @@
 "use strict";
-//Typescript singelton instance using class
-class Person {
-    constructor() { }
-    static getInstance() {
-        if (Person._instance) {
-            return Person._instance;
-        }
-        Person._instance = new Person();
-        return Person._instance;
+//Typescript Interface
+// let user1: User;
+// user1 = {
+//   firstName: "John",
+//   lastName: "Smith",
+//   greetUser() {
+//     console.log("hello user");
+//   },
+//   getFullName() {
+//     return this.firstName + this.lastName;
+//   },
+// };
+//class Admin implements User, Roles {} // can pass multiple interface. Class should have all the method and proeprty of the interface. but have additional method or property as well ....
+class Admin {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = 40;
+    }
+    greetUser() {
+        console.log("hello admin: " + this.getFullName());
+    }
+    getFullName() {
+        return this.firstName + this.lastName;
     }
 }
-const person1 = Person.getInstance(); //will give first instance
-const person2 = Person.getInstance(); //will again give first instance , not new instance
+class Member {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = 20;
+    }
+    greetUser() {
+        console.log("hello member: " + this.getFullName());
+    }
+    getFullName() {
+        return this.firstName + this.lastName;
+    }
+}
+function displayGreetMessage(user) {
+    user.greetUser();
+}
+const admin = new Admin("Abdullah", "Limon");
+const member = new Member("Rabeya", "Akbor");
+displayGreetMessage(admin);
+displayGreetMessage(member);
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,60 @@
-//Typescript singelton instance using class
+//Typescript Interface
 
-class Person {
-  private static _instance: Person;
+interface Person {
+  age: number;
+}
 
-  private constructor() {}
-  static getInstance() {
-    if (Person._instance) {
-      return Person._instance;
-    }
-    Person._instance = new Person();
-    return Person._instance;
+interface User extends Person {
+  firstName: string;
+  lastName: string;
+
+  greetUser(): void;
+  getFullName(): string;
+} //interface support interfce inheritance but type is not support that
+
+// let user1: User;
+
+// user1 = {
+//   firstName: "John",
+//   lastName: "Smith",
+//   greetUser() {
+//     console.log("hello user");
+//   },
+//   getFullName() {
+//     return this.firstName + this.lastName;
+//   },
+// };
+
+//class Admin implements User, Roles {} // can pass multiple interface. Class should have all the method and proeprty of the interface. but have additional method or property as well ....
+
+class Admin implements User {
+  age: number = 40;
+  constructor(public firstName: string, public lastName: string) {}
+  greetUser() {
+    console.log("hello admin: " + this.getFullName());
+  }
+  getFullName() {
+    return this.firstName + this.lastName;
   }
 }
 
-const person1 = Person.getInstance(); //will give first instance
+class Member implements User {
+  age: number = 20;
+  constructor(public firstName: string, public lastName: string) {}
+  greetUser() {
+    console.log("hello member: " + this.getFullName());
+  }
+  getFullName() {
+    return this.firstName + this.lastName;
+  }
+}
 
-const person2 = Person.getInstance(); //will again give first instance , not new instance
+function displayGreetMessage(user: User) {
+  user.greetUser();
+}
+
+const admin = new Admin("Abdullah", "Limon");
+const member = new Member("Rabeya", "Akbor");
+
+displayGreetMessage(admin);
+displayGreetMessage(member);
